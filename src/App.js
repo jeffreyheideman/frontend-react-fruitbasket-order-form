93a87211b633ef64fc5cfbbab402720fa1cc8e7f
jeffreyheideman <jeffrey_heideman@hotmail.com>
2023-05-17 14:43:49 +0200

    Opdracht 1 is af

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
-import Fruitcounter from "./components/Fruitcounter";
+
 
 
 function App() {
@@ -9,39 +9,56 @@ function App() {
     const [appleCounter, setAppleCounter] = useState(0);
     const [kiwiCounter, setKiwiCounter] = useState(0);
 
+    function decreaseCounter(counter, setCounter) {
+        if (counter > 0) {
+            setCounter( counter - 1)
+        }
+    }
+
     return (
         <>
             <h1>Fruitmand bezorgservice</h1>
-            <Fruitcounter
-                name="Aardbeien"
-                counter={strawberryCounter}
-                setCounter={setStrawberryCounter}/>
+            <div>
+                <h2>Aardbeien</h2>
+                <button type="button" onClick={() => setStrawberryCounter(strawberryCounter + 1)}>+</button>
+                <p>{strawberryCounter}</p>
+                <button type="button" onClick={() => decreaseCounter(strawberryCounter, setStrawberryCounter)}>-</button>
+            </div>
 
-            <Fruitcounter
-                name="Bananen"
-                counter={bananaCounter}
-                setCounter={setBananaCounter}/>
+            <div>
+                <h2>Bananen</h2>
+                <button type="button" onClick={() => setBananaCounter(bananaCounter + 1)}>+</button>
+                <p>{bananaCounter}</p>
+                <button type="button" onClick={() => decreaseCounter(bananaCounter, setBananaCounter())}>-</button>
+            </div>
 
-            <Fruitcounter
-                name="Appels"
-                counter={appleCounter}
-                setCounter={setAppleCounter}/>
+            <div>
+                <h2>Appels</h2>
+                <button type="button" onClick={() => setAppleCounter(appleCounter + 1)}>+</button>
+                <p>{appleCounter}</p>
+                <button type="button" onClick={() => decreaseCounter(appleCounter, setAppleCounter())}>-</button>
+            </div>
 
-            <Fruitcounter
-                name="Kiwi's"
-                counter={kiwiCounter}
-                setCounter={setKiwiCounter}/>
+            <div>
+                <h2>Kiwi's</h2>
+                <button type="button" onClick={() => setKiwiCounter(kiwiCounter + 1)}>+</button>
+                <p>{kiwiCounter}</p>
+                <button type="button" onClick={() => decreaseCounter(kiwiCounter, setKiwiCounter())}>-</button>
+            </div>
 
 
 
             <button
                 type="button"
-                onClick={() => {setStrawberryCounter(0)
+                onClick={() => {
+                    setStrawberryCounter(0)
                     setBananaCounter(0)
                     setAppleCounter(0)
                     setKiwiCounter(0)}}
+            >Reset
+            </button>
 
-            >Reset</button>
+
         </>
     );
 }
